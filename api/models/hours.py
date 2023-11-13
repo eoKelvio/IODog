961b03b4e3fa2c104d0e228c5c72ff18eff95f7e
Kelvin Silvestre <kelvinsc7@gmail.com>
2023-11-13 17:44:47 -0300

    feat: Model Hour created

---
+++ api/models/hours.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Hours(models.Model):
+    times = models.CharField(max_length=10, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return super().__str__()
