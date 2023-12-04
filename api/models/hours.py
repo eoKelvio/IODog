5093069930cc42b00c5f9bb7db16315ed76508ce
eoKelvio <kelvinsc7@gmail.com>
2023-12-04 18:38:04 +0000

    patch: 1.1

--- api/models/hours.py
+++ api/models/hours.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Hours(models.Model):
-    times = models.CharField(max_length=30, null=False, blank=False)
+    times = models.CharField(max_length=30, null=False, blank=False, unique=True)
 
     def __str__(self) -> str:
         return super().__str__()
